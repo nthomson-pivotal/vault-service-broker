ee32fd1a4059e90a6bd9eec2c3d8d5bb5d6de3a8
Becca Petrin <beccapetrin@posteo.net>
2019-01-11 16:24:54 -0800

    fix policies

--- vault.go
+++ vault.go
@@ -34,44 +34,51 @@ path "cf/{{ .OrganizationGUID }}/*" {
 
 	// ServicePolicyTemplateWithNames is identical to the above, but adds paths for name-ID mount path combos
 	ServicePolicyTemplateWithNames string = `
-path "cf/{{ .ServiceInstanceName }}-{{ .ServiceInstanceGUID }}" {
-  capabilities = ["list"]
-}
 path "cf/{{ .ServiceInstanceGUID }}" {
   capabilities = ["list"]
 }
-path "cf/{{ .ServiceInstanceName }}-{{ .ServiceInstanceGUID }}/*" {
-	capabilities = ["create", "read", "update", "delete", "list"]
+
+path "cf/{{ .ServiceInstanceName }}-{{ .ServiceInstanceGUID }}" {
+  capabilities = ["list"]
 }
+
 path "cf/{{ .ServiceInstanceGUID }}/*" {
 	capabilities = ["create", "read", "update", "delete", "list"]
 }
-path "cf/{{ .SpaceName }}-{{ .SpaceGUID }}" {
-  capabilities = ["list"]
+
+path "cf/{{ .ServiceInstanceName }}-{{ .ServiceInstanceGUID }}/*" {
+	capabilities = ["create", "read", "update", "delete", "list"]
 }
+
 path "cf/{{ .SpaceGUID }}" {
   capabilities = ["list"]
 }
-path "cf/{{ .SpaceName }}-{{ .SpaceGUID }}/*" {
-  capabilities = ["create", "read", "update", "delete", "list"]
+
+path "cf/{{ .SpaceName }}-{{ .SpaceGUID }}" {
+  capabilities = ["list"]
 }
+
 path "cf/{{ .SpaceGUID }}/*" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
-path "cf/{{ .OrganizationName }}-{{ .OrganizationGUID }}" {
-  capabilities = ["list"]
+
+path "cf/{{ .SpaceGUID }}-{{ .SpaceGUID }}/*" {
+  capabilities = ["create", "read", "update", "delete", "list"]
 }
+
 path "cf/{{ .OrganizationGUID }}" {
   capabilities = ["list"]
 }
-	// SpaceID is the unique ID of the space.
-	SpaceID string
-path "cf/{{ .OrganizationName }}-{{ .OrganizationGUID }}/*" {
+
+path "cf/{{ .OrganizationName }}-{{ .OrganizationGUID }}" {
+  capabilities = ["list"]
+}
+
+path "cf/{{ .OrganizationGUID }}/*" {
   capabilities = ["read", "list"]
 }
-	// OrgID is the unique ID of the space.
-	OrgID string
-path "cf/{{ .OrganizationGUID }}/*" {
+
+path "cf/{{ .OrganizationName }}-{{ .OrganizationGUID }}/*" {
   capabilities = ["read", "list"]
 }
 `
